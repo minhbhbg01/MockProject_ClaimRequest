d835829b0ad55d6baa9a95a969ccf99de40bf7ad
Minh <minhbh2000@gmail.com>
2024-01-30 15:09:36 +0700

    create mock project

---
+++ src/main/java/fa/edu/MockProjectClaimRequestApplication.java
@@ -0,0 +1,13 @@
+package fa.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockProjectClaimRequestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockProjectClaimRequestApplication.class, args);
+    }
+
+}
